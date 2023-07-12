a5564102a4608d73d2de4259399115a2ae5e0cba
Guilherme Sella <guilhermesella1974@gmail.com>
2023-07-12 15:48:49 -0300

    create LoadCoordinates function

--- src/pages/Saved/Saved.jsx
+++ src/pages/Saved/Saved.jsx
@@ -1,11 +1,33 @@
 import React, { useEffect } from 'react';
 import { Div } from '../../components/Saved/Saved.style';
+import { doc, onSnapshot, collection, query, where } from 'firebase/firestore';
+import { db } from '../../services/Firebase-connection';
 
 
 function Saved(props) {
     
     useEffect(()=>{
-        async function LoadCoordinates
+        async function LoadCoordinates(){
+            const obj = JSON.parse(localStorage.getItem("@userStorage"))
+            const coordinatesReF = collection(db, "coordinates")
+            const q = query(coordinatesReF, where("userId", "==", obj.uid))
+            const unsub = onSnapshot(q, (snapshot)=>{
+                let list = [];
+                snapshot.forEach((item)=>{
+                    list.push({
+                        id:item.id,
+                        adress:item.data().adress,
+                        lat:item.data().lat,
+                        lng:item.data().lng,
+                        srcMap:item.data().srcMap
+                    })
+                })
+                console.log(list)
+            })
+        }
+
+        LoadCoordinates()
+
     }, [])
 
 
