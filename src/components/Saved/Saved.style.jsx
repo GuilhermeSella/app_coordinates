bab484999271cb94fdae1f6a6d897c8fc1571051
Guilherme Sella <guilhermesella1974@gmail.com>
2023-07-18 15:21:43 -0300

    Style not found save page

--- src/components/Saved/Saved.style.jsx
+++ src/components/Saved/Saved.style.jsx
@@ -1,19 +1,23 @@
 import styled from "styled-components";
 
 export const Div = styled.main`
-    height: ${(prop)=> prop.loading === true ? "100vh" : "none" };
+    height: ${(prop)=> prop.loading === true ? "100vh" : "1" };
     width: 100%;
     background-color: ${(prop)=>prop.theme === "light" ? "white" : "#0a0a0aea"};
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 30px;
-    padding-bottom: 10vh;
+    padding-bottom: 40vh;
+    padding-top: 5vh;
     color: ${(prop)=>prop.theme === "light" ? "black" : "white"};
 
     h1{
         padding-bottom: 4vh;
        
+    }
+    h1 b{
+        color: #9E30F4;
     }
 
     .container{
@@ -55,6 +59,27 @@ export const Div = styled.main`
         text-decoration: underline;
     }
     .container .options a{
+        color: #9E30F4;
+        font-weight: bold;
+    }
+`
+
+export const DivnotFound = styled.div`
+    display:flex ;
+    flex-direction: column;
+    align-items: center;
+    padding: 20vh 0;
+    text-align: center;
+    img{
+        width: 90vw;
+        
+        max-width: 400px;
+    }
+
+    h1{
+
+    }
+    a{
         color: #9E30F4;
         font-weight: bold;
     }
--- src/pages/Saved/Saved.jsx
+++ src/pages/Saved/Saved.jsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { Div } from '../../components/Saved/Saved.style';
+import { Div, DivnotFound } from '../../components/Saved/Saved.style';
 import { doc, onSnapshot, collection, query, where, limit } from 'firebase/firestore';
 import { db } from '../../services/Firebase-connection';
 import { useContext } from 'react';
 import {ThemeContext} from '../../contexts/Theme'
+import imgnotfound from './imgnotfound.png'
 
 function Saved(props) {
 
     const {theme} = useContext(ThemeContext)
     const [limit, setLimit] = useState(5)
+    const [notFound, setNotFound] = useState(false)
     const [loading, setLoading] = useState(true)
     const [listCoordinates, setListCordinates] = useState([])
     
+
+    async function deleteCoordinates(Id){
+        
+    }
+
     useEffect(()=>{
         async function LoadCoordinates(){
             const obj = JSON.parse(localStorage.getItem("@userStorage"))
@@ -28,7 +35,13 @@ function Saved(props) {
                         srcMap:item.data().srcMap
                     })
                 })
+                
                 setListCordinates(list)
+                if(list.length == 0){
+                    setNotFound(true)
+                }else{
+                    setNotFound(false)
+                }
                 setLoading(false)
             })
         }
@@ -37,26 +50,35 @@ function Saved(props) {
 
     }, [])
 
+  
+
 
     return (
         <>
       
             <Div loading = {loading} theme={theme}>
-            <h1>Página Salvos</h1>
+            <h1>Página <b>Salvos</b></h1>
            {loading === true ? (
                <h2>Buscando Dados...</h2>
+           ) : notFound === true ? (
+                <DivnotFound >
+                    <h1 >Nenhum registro encontrado!</h1>
+                    
+                    <a href="/coordinates">Começar</a>
+                    
+                </DivnotFound>
            ) : listCoordinates.map((item)=>(
-               <div className='container' key={item.id}>
-                   <h2>Adress: <b>{item.adress}</b></h2>
-                   <p>Latitude: <b>{item.lat}</b></p>
-                   <p>Longitude: <b>{item.lng}</b></p>
-                   <div className='options'>
-                       <button>Excluir</button>
-                        <a href="">Mapa</a>
-                   </div>
-               </div>
+            <div className='container' key={item.id}>
+                <h2>Adress: <b>{item.adress}</b></h2>
+                <p>Latitude: <b>{item.lat}</b></p>
+                <p>Longitude: <b>{item.lng}</b></p>
+                <div className='options'>
+                    <button onClick={deleteCoordinates(item.id)}>Excluir</button>
+                     <a target='_blank' href={`https://www.google.com/maps/search/?api=1&query=${item.lat}%2C${item.lng}`}>Maps</a>
+                </div>
+            </div>
 
-           ))}
+        ))}
        </Div>
         </>
     );
